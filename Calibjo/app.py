1c03e5b9c904f317d57d470f2e8d58b8c47ac41f
JOHNfreedom1234 <joshuaprinze.calibjo@wvsu.edu.ph>
2025-02-26 11:10:20 +0800

    Implement 1

--- Calibjo/app.py
+++ Calibjo/app.py
@@ -5,13 +5,13 @@ from flask_cors import CORS
 import json
 from pydantic import BaseModel
 
-class Diagnosis (BaseModel)
+class Diagnosis (BaseModel):    
     key_id: str
     primary_name: str
     consumer_name: str
     word_synonyms: str
     synonyms: list[str]
-    info_link_data: list[str]
+    info_link_data: list[list[str]]
 
 load_dotenv()
 
@@ -31,7 +31,22 @@ with open("diseases.json", "r") as f:
 def home():
     return render_template({"index.html"})
 
-@app.route
+@app.route('/diseases', methods=['GET'])
+def get_diseases():
+ return jsonify(diseases)
+ 
+@app.route('/chat', methods=['GET'])
+def get_chat():
+ response = client.models.generate_content(
+ model="gemini-2.0-flash",
+ contents=[
+ "You a disease diagnosing staff",
+ "Create a basic diagnosis for a patient with the following symptoms: black skin spots, swelling, fever, vomiting, and headache.",
+ "The patient have been experiencing the symptoms for a week already",
+ "Provide an accurate diagnosis"
+ ])
+ 
+ return response.text
 
 @app.route("/diagnostic", methods=["GET"])
 def search():
@@ -39,8 +54,15 @@ def search():
     response = client.models.generate_content
     model = "gemini-2.0-flash",
     contents=[
-        "This is the existing data in JSON format"
-    ]
+        "This is the existing data in JSON format; " + json.dumps(diseases),
+        "Match the closest disease with following symptoms: " + symptoms,
+        "Include the info link data in the response.",
+        "Return the top three matching items."
+    ],
+    config={
+        "response_mime_type": "application/json",
+        "response schema": list[Diagnosis]
+    }
     
 
 if __name__ == "__main__":
