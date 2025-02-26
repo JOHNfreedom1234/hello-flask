c4173a9c1668676e0a011d12f048a44830ef8ab7
JOHNfreedom1234 <joshuaprinze.calibjo@wvsu.edu.ph>
2025-02-26 10:56:27 +0800

    Save

--- Calibjo/app.py
+++ Calibjo/app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv, dotenv_values
+from google import genai
 from flask_cors import CORS
 import json
+from pydantic import BaseModel
+
+class Diagnosis (BaseModel)
+    key_id: str
+    primary_name: str
+    consumer_name: str
+    word_synonyms: str
+    synonyms: list[str]
+    info_link_data: list[str]
+
+load_dotenv()
+
+config = dotenv_values(".env")
+
+client = genai.client(api_key=config['GEMINI_API_KEY'])
 
 app = Flask(__name__)
 CORS(app)
+
+diseases = []
 
 with open("diseases.json", "r") as f:
     diseases = json.load(f)
@@ -12,33 +31,17 @@ with open("diseases.json", "r") as f:
 def home():
     return render_template({"index.html"})
 
-@app.route("/diagnostic", methods=["POST", "GET"])
+@app.route
+
+@app.route("/diagnostic", methods=["GET"])
 def search():
-    search_symptoms = []
-
-    if request.method == "GET":
-        symptoms = request.args.get("symptoms", "").lower()
-        if symptoms:
-            search_symptoms = symptoms.split(",")
-
-    elif request.method == "POST":
-        search_symptoms = request.json.get("symptoms",[])
-
+    symptoms = request.args.get('symptoms', '').lower()
+    response = client.models.generate_content
+    model = "gemini-2.0-flash",
+    contents=[
+        "This is the existing data in JSON format"
+    ]
     
-    matching_diseases = []
-    for disease, info in diseases.items():
-        synoyms = info.get("Synonyms","").split(";")
-        match_symptoms = set(synoyms) & set(search_symptoms)
-        if match_symptoms:
-            matching_diseases.append({
-                "name": disease,
-                "match_count": len(match_symptoms),
-                "info_link": info.get("info_link_data",[]),
-                "icd10_codes": info.get("icd10cm_codes", ""),
-            })
-    
-    matching_diseases.sort(key=lambda x: x["match_count"], reverse=True)
-    return jsonify(matching_diseases)
 
 if __name__ == "__main__":
     app.run(debug=True)
